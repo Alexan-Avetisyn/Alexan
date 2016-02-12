941ba67ec25e2950cc54a13adf3cabb504ef48d0
unknown <july.ignatjeva1994@mail.ru>
2016-02-12 16:25:42 +0300

    lesson2

---
+++ lesson2.R
@@ -0,0 +1,8 @@
+f=rep(c("mouse","rat","fly"),c(5,7,11))
+x=factor(f)
+summary(f)
+summary(x)
+summary(1:100)
+summary(1:100)[1]
+dna=rep(c("A","T","G","C"),c("1382","586","725","1708"))
+sample(dna,size=84000,replace=TRUE)
